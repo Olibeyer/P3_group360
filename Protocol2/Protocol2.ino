5d55d1aa0faa6df9e49571e7efcb6bc6c14fc175
Hans Larsen <hans.larsen1994@gmail.com>
2019-09-30 15:29:44 +0200

    Added a major fuckup.

---
+++ Protocol2/Protocol2.ino
@@ -0,0 +1,52 @@
+#include "Protocol_2.h"
+
+ProtocolController* controler_ptr;
+
+long oldTime = 0;
+int setPos = 0;
+int a = 0;
+bool up = true;
+void setup() {
+  controler_ptr = new ProtocolController();
+  Serial.begin(115200);
+  pinMode(13, OUTPUT);
+  digitalWrite(13, HIGH);
+
+  /*for (int i = 1; i <= 5; i++) {
+    controler_ptr->toggleTorque(i, true);
+    delay(10);
+  }*/
+
+  delay(100);
+
+  
+  controler_ptr->ping(0x01);
+}
+
+void loop() {
+  if (millis()-oldTime > 10000) {
+    oldTime = millis();
+    //controler_ptr->ping(0xFE);
+    
+    /*
+    if(up)
+      a += 10;
+    else
+      a -= 10;
+
+    if(a > 300)
+      up = !up;
+    if(a < 0)
+      up = !up;
+      
+
+    controler_ptr->setPos(0x01, 1300 + a);
+      
+    controler_ptr->setPos(0x02, 500 + a);
+    controler_ptr->setPos(0x03, 2000 + a);
+      
+    controler_ptr->setPos(0x04, 1200 + a);
+    controler_ptr->setPos(0x05, 2300 - a);*/
+    digitalWrite(13, !digitalRead(13));
+  }
+}
---
+++ Protocol2/Protocol_2.h
@@ -0,0 +1,31 @@
+#ifndef PROTOCAL_H
+#define PROTOCAL_H
+
+#include <Arduino.h>;
+
+const int sRecRX = 2;
+const int sTranTX = 3;
+const int SControl = 4;   //RS485 Direction control
+const int Pin13LED = 13;
+const int Baudrate = 57600;
+
+class ProtocolController {
+public:
+  ProtocolController();
+  void ping(unsigned char address);
+  void ledSet(unsigned char address, bool ledStatus);
+  void toggleTorque(unsigned char address, bool onTrue);
+  void setPos(unsigned char address, long setPosition);
+  bool writeFunction(unsigned char address, unsigned char *data_blk_ptr, unsigned short data_blk_size);
+  void update_receiver();
+
+  bool writeReturn = false;
+
+  unsigned char packageBuffer[255];
+  int packageBufferLength = 0;
+
+private: 
+  unsigned short update_crc(unsigned short crc_accum, unsigned char *data_blk_ptr, unsigned short data_blk_size);
+};
+
+#endif

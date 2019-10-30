c25f4af4b42748608d3dd8d2769c2670b402402a
Anders-Clement <andersclement96@gmial.com>
2019-10-30 10:32:14 +0100

    added catkin_ws

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/clement/dev/P3_group360/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/clement/p1_ws/mrrr/devel;/home/clement/turtlebot/devel;/home/clement/ros_ws/devel;/opt/ros/melodic".split(';') if "/home/clement/p1_ws/mrrr/devel;/home/clement/turtlebot/devel;/home/clement/ros_ws/devel;/opt/ros/melodic" != "" else []

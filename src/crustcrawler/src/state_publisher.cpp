311f5fea0c1841ae5c9a4737b555c2a8264b70e3
Anders-Clement <andersclement96@gmial.com>
2019-11-04 10:12:17 +0100

    added rviz launch options

--- src/crustcrawler/src/state_publisher.cpp
+++ src/crustcrawler/src/state_publisher.cpp
@@ -7,52 +7,50 @@ int main(int argc, char** argv) {
     ros::init(argc, argv, "state_publisher");
     ros::NodeHandle n;
     ros::Publisher joint_pub = n.advertise<sensor_msgs::JointState>("joint_states", 1);
-    tf::TransformBroadcaster broadcaster;
     ros::Rate loop_rate(30);
 
     const double degree = M_PI/180;
 
-    // robot state
-    double tilt = 0, tinc = degree, swivel=0, angle=0, height=0, hinc=0.005;
-
     // message declarations
-    geometry_msgs::TransformStamped odom_trans;
     sensor_msgs::JointState joint_state;
-    odom_trans.header.frame_id = "base_link";
-    odom_trans.child_frame_id = "arm";
+
+    float pos = 0;
+    bool up = true;
+
 
     while (ros::ok()) {
         //update joint_state
         joint_state.header.stamp = ros::Time::now();
-        joint_state.name.resize(3);
-        joint_state.position.resize(3);
-        joint_state.name[0] ="base";
-        joint_state.position[0] = 0;//swivel;
-        joint_state.name[1] ="shoulder";
-        joint_state.position[1] = 0;//tilt;
-        joint_state.name[2] ="elbow";
-        joint_state.position[2] = 0;//height;
+        joint_state.name.resize(5);
+        joint_state.position.resize(5);
+        joint_state.name[0] ="joint1";
+        joint_state.position[0] = pos;
+        joint_state.name[1] ="joint2";
+        joint_state.position[1] = 0;
+        joint_state.name[2] ="joint3";
+        joint_state.position[2] = 0;
+        joint_state.name[3] ="joint4";
+        joint_state.position[3] = 0;
+        joint_state.name[4] ="joint5";
+        joint_state.position[4] = 0;
 
 
-        // update transform
-        // (moving in a circle with radius=2)
-        odom_trans.header.stamp = ros::Time::now();
-        odom_trans.transform.translation.x = 0;
-        odom_trans.transform.translation.y = 0;
-        odom_trans.transform.translation.z = 0.7;
-        odom_trans.transform.rotation = tf::createQuaternionMsgFromYaw(0);
+        if(pos > 3.14)
+          up = !up;
+        if(pos < 0)
+          up = !up;
+
+        if(up)
+          pos += 0.1 ;
+        else
+          pos -= 0.1;
+
+
 
         //send the joint state and transform
         joint_pub.publish(joint_state);
-        broadcaster.sendTransform(odom_trans);
 
-        // Create new robot state
-        tilt += tinc;
-        if (tilt<-.5 || tilt>0) tinc *= -1;
-        height += hinc;
-        if (height>.2 || height<0) hinc *= -1;
-        swivel += degree;
-        angle += degree/4;
+
 
         // This will adjust as needed per iteration
         loop_rate.sleep();

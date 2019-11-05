59f64b3672dfc9916992c37bbbbb8b20d6c5c38e
Jonas Andersen <andersen11@andersen12.localdomain>
2019-11-05 11:14:52 +0100

    changed the gest_sub to gest_str_sub and made it work with the rviz sim

--- src/crustcrawler/src/state_publisher.cpp
+++ src/crustcrawler/src/state_publisher.cpp
@@ -1,14 +1,16 @@
 #include <string>
 #include <ros/ros.h>
 #include <sensor_msgs/JointState.h>
-#include <geometry_msgs/PoseStamped.h>
+#include <std_msgs/String.h>
 #include <ros_myo/MyoPose.h>
 #include <tf/transform_broadcaster.h>
 
+using namespace std;
 
 float angles[3];
 uint8_t gesture = 0;
 
+//Takes the quaternions from the imu and calculates the roll, pitch and yaw
 void myo_raw_pose_callback(const geometry_msgs::PoseStamped::ConstPtr& msg){
   float x = msg->pose.orientation.x;
   float y = msg->pose.orientation.y;
@@ -39,17 +41,18 @@ void myo_raw_pose_callback(const geometry_msgs::PoseStamped::ConstPtr& msg){
   angles[2] = yaw;
 }
 
-void myo_raw_gest_callback(const ros_myo::MyoPose::ConstPtr& msg){
-  gesture = msg->pose;
-  /*
-  UNKNOWN = 0
-  REST = 1
-  FIST = 2
-  WAVE_IN = 3
-  WAVE_OUT = 4
-  FINGERS_SPREAD = 5
-  THUMB_TO_PINKY = 6
-  */
+//checks what gesture the myo detects
+void myo_raw_gest_str_callback(const std_msgs::String::ConstPtr& msg){
+  string data = msg->data;
+  const string known_gestures[] = {"UNKNOWN","REST","FIST","FINGERS_SPREAD","WAVE_IN","WAVE_OUT","THUMB_TO_PINKY"};
+
+  for(int i = 0; i < 6; i++){
+    if(data == known_gestures[i]){
+      gesture = i;
+      ROS_INFO_STREAM(data);
+      return;
+    }
+  }
 }
 
 
@@ -59,7 +62,7 @@ int main(int argc, char** argv) {
 
   ros::Publisher joint_pub = n.advertise<sensor_msgs::JointState>("joint_states", 1);
   ros::Subscriber pose_sub = n.subscribe("myo_raw/pose", 10, myo_raw_pose_callback);
-  ros::Subscriber gest_sub = n.subscribe("myo_raw/gest", 10, myo_raw_gest_callback);
+  ros::Subscriber gest_str_sub = n.subscribe("myo_raw/myo_gest_str", 10, myo_raw_gest_str_callback);
 
   ros::Rate loop_rate(30);
 
@@ -68,8 +71,8 @@ int main(int argc, char** argv) {
   // message declarations
   sensor_msgs::JointState joint_state;
 
-  uint8_t pos3 = 0;
-  uint8_t grip = 0;
+  float pos3 = 0;
+  float grip = 0;
 
   while (ros::ok()) {
     //update joint_state
@@ -87,26 +90,25 @@ int main(int argc, char** argv) {
     joint_state.name[4] ="joint5";
     joint_state.position[4] = -grip;
 
-    if (gesture == 3) {
-      pos3 += 0.1;
-    }
-    else if (gesture == 4) {
-      pos3 -= 0.1;
-    }
-
-    if (gesture == 1) {
-      grip += 0.1;
+    if (gesture == 4) {
+      pos3 += 0.02;
     }
     else if (gesture == 5) {
-      grip -= 0.1;
+      pos3 -= 0.02;
     }
 
+    if (gesture == 2) {
+      grip += 0.02;
+    }
+    else if (gesture == 3) {
+      grip -= 0.02;
+    }
 
 
     //send the joint state and transform
     joint_pub.publish(joint_state);
 
-
+    // updeate and check for news in the subscribers
     ros::spinOnce();
 
     // This will adjust as needed per iteration
